9489d64ba4b4c7b470a854a704432a28b7c44c0f
Auritro Dey Kirty <deykirtyauritro@gmail.com>
2026-02-26 00:23:51 +0530

    Changes

---
+++ Digital Clock/script.js
@@ -0,0 +1,15 @@
+
+function timer(){
+    const time = new Date();
+    let hours = time.getHours();
+    const meridian = hours >=12 ? "PM": "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, 0);
+    const minutes = time.getMinutes().toString().padStart(2, 0);
+    const seconds = time.getSeconds().toString().padStart(2, 0);
+    const textstring = `${hours}:${minutes}:${seconds} ${meridian}`;
+    document.getElementById("clock").textContent = textstring;
+}
+
+timer();
+setInterval(timer, 1000);
